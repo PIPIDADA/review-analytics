0166e6b67a455a04a3f12b9472599ceacec79946
PIPIDADA <archer021010@gmail.com>
2019-04-21 12:51:16 +0800

    calc analys mean len

--- Read_file.py
+++ Read_file.py
@@ -1,12 +1,26 @@
 # 留言分析程式
 
 data = []
-count = 0
+k = 0
+words = []
 with open('reviews.txt', 'r') as f :
 	for line in f :
 		data.append(line)
-		count += 1
-		if count % 10000 == 0:
+		k += 1
+		if k % 100000 == 0: #餘數為0的意思
 			print(len(data))
-print(data[0])
+print('資料搜索完畢，共有', len(data), '筆資料！')
 
+# 自己寫的。很慢！！！
+for letters in data:
+	for word in letters:
+		words.append(word)
+
+Mean_words = len(words) / len(data)
+print('留言平均長度為：', Mean_words, '字')
+
+# 答案簡單版。很快！！！  
+sum_len = 0
+for letters in data:
+	sum_len = sum_len + len(letters)
+print('留言平均長度為：',sum_len / len(data) , '字')
