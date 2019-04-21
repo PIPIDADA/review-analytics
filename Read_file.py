50d1014e4452a0d3790f8a5ad74c8f13cb1e9512
PIPIDADA <archer021010@gmail.com>
2019-04-21 14:46:49 +0800

    add filtering

--- Read_file.py
+++ Read_file.py
@@ -12,15 +12,32 @@ with open('reviews.txt', 'r') as f :
 print('資料搜索完畢，共有', len(data), '筆資料！')
 
 # 自己寫的。很慢！！！
-for letters in data:
-	for word in letters:
-		words.append(word)
+# for letters in data:
+# 	for word in letters:
+# 		words.append(word)
 
-Mean_words = len(words) / len(data)
-print('留言平均長度為：', Mean_words, '字')
+# Mean_words = len(words) / len(data)
+# print('留言平均長度為：', Mean_words, '字')
 
 # 答案簡單版。很快！！！  
 sum_len = 0
 for letters in data:
 	sum_len = sum_len + len(letters)
 print('留言平均長度為：',sum_len / len(data) , '字')
+
+# 篩選
+new = []
+for d in data:
+	if len(d) < 100 :
+		new.append(d)
+print(len(new))
+print(new[0])
+
+
+
+
+
+
+
+
+
